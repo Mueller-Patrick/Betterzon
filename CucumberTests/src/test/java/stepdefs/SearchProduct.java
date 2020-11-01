fcd28648a8b8e50b1540c10f9bc3b6ffd07e730a
Patrick <50352812+Mueller-Patrick@users.noreply.github.com>
2020-11-01 22:09:17 +0100

    Reverting revert 742e86611d98b176569356090cedc6e66d94e724 of commit 6ff8caf6f36aee4b79fbe777334a0b1a3e9581da

--- CucumberTests/src/test/java/stepdefs/SearchProduct.java
+++ CucumberTests/src/test/java/stepdefs/SearchProduct.java
@@ -1,7 +1,6 @@
 package stepdefs;
 
 import cucumber.api.PendingException;
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -19,11 +18,35 @@ public class SearchProduct {
   public void the_user_should_see_the_error_page(String arg0) throws Exception {
   }
 
-  @And("^the user is not logged in$")
+  @Given("^the user is not logged in$")
   public void the_user_is_not_logged_in() throws Exception {
   }
 
-  @And("^the user is logged in$")
+  @Given("^the user is logged in$")
   public void the_user_is_logged_in() throws Exception {
+  }
+
+  @Then("^the user should see a list of products$")
+  public void the_user_should_see_a_list_of_products() throws Exception {
+  }
+
+  @When("^the user clicks on the first product$")
+  public void the_user_clicks_on_the_first_product() throws Exception {
+  }
+
+  @Then("^the user should see the product detail page$")
+  public void the_user_should_see_the_product_detail_page() throws Exception {
+  }
+
+  @Then("^the set price alarm box should show \"([^\"]*)\"$")
+  public void the_set_price_alarm_box_should_show(String arg0) throws Exception {
+  }
+
+  @When("^the user sets a price alarm$")
+  public void the_user_sets_a_price_alarm() throws Exception {
+  }
+
+  @Then("^the user should receive an email confirming the price alarm$")
+  public void the_user_should_receive_an_email_confirming_the_price_alarm() throws Exception {
   }
 }
