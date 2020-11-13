7ae9524d064ea44ed0c7652751d0710d78ce17c9
Patrick Müller <patrick@mueller-patrick.tech>
2020-11-13 20:59:16 +0100

    BETTERZON-29: Adding hyperlink to blog just for fun

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
