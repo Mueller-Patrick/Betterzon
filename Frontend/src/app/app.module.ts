a9b60ad0f5089a8e9c3b68a7dcd8ad744a5db1e0
Patrick Müller <patrick@mueller-patrick.tech>
2020-11-15 11:07:43 +0100

    BETTERZON-29: Adding module .iml files

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent,
-    TestComponent
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule
