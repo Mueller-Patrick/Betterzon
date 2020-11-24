6b8acdab469b5b6cf23dee796f1e9c0683abbc2a
Patrick Müller <patrick@mueller-patrick.tech>
2020-11-25 00:38:16 +0100

    BETTERZON-38: Added error handler, preparation for DB accessing

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -7,6 +7,8 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { productsRouter } from "./products/products.router";
+import { errorHandler } from "./middleware/error.middleware";
+import {notFoundHandler} from "./middleware/notFound.middleware";
 
 dotenv.config();
 
@@ -32,6 +34,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use("/products", productsRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 
 /**
