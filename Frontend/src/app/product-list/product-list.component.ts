23ae0555e5a95e3b8215b3542b3cf8792a823a03
Patrick Müller <patrick@mueller-patrick.tech>
2020-12-02 14:14:49 +0100

    BETTERZON-33: Styles and redirect upon click on product

--- Frontend/src/app/product-list/product-list.component.ts
+++ Frontend/src/app/product-list/product-list.component.ts
@@ -1,8 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import axios, {AxiosInterceptorManager, AxiosRequestConfig, AxiosResponse} from 'axios';
-import process from 'process';
 import {ApiService} from '../api.service';
 import {Product} from '../models/product';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-product-list',
@@ -12,7 +11,10 @@ import {Product} from '../models/product';
 export class ProductListComponent implements OnInit {
     products: Product[];
 
-    constructor(private apiService: ApiService) {
+    constructor(
+        private apiService: ApiService,
+        private router: Router
+    ) {
     }
 
     ngOnInit(): void {
@@ -21,6 +23,10 @@ export class ProductListComponent implements OnInit {
 
     getProducts(): void {
         this.apiService.getProducts().subscribe(products => this.products = products);
+    }
+
+    clickedProduct(product: Product): void {
+        this.router.navigate([('/helloworld/' + product.product_id)]);
     }
 
 }
