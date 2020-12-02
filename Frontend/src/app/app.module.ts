1037a5fdcf5b92426b18026cb8201db0f917448a
rebooooorn <jegor.reichert.1998@gmail.com>
2020-12-02 18:49:46 +0100

    BETTERZON-35 components created

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    ProductDetailPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
