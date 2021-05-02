e9d03b9cbb9346a1b25a404bf5ea802347fc2aa1
Patrick <50352812+Mueller-Patrick@users.noreply.github.com>
2021-05-02 15:58:47 +0200

    BETTERZON-83: FE unit testing (#35)
    * BETTERZON-83: Making pre-generated unit tests work
    
    * BETTERZON-83: Writing unit tests for angular to improve code coverage

--- Frontend/src/app/components/header/header.component.spec.ts
+++ Frontend/src/app/components/header/header.component.spec.ts
@@ -1,25 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import {HeaderComponent} from './header.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {MatMenuModule} from "@angular/material/menu";
+import {Router} from "@angular/router";
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+    let component: HeaderComponent;
+    let fixture: ComponentFixture<HeaderComponent>;
+    let router = {
+        navigate: jasmine.createSpy('navigate'),
+        navigateByUrl: (url: string) => {
+            return {
+                then: () => {}
+            }
+        }
+    }
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            providers: [{provide: Router, useValue: router}],
+            declarations: [HeaderComponent],
+            imports: [
+                RouterTestingModule,
+                MatMenuModule
+            ]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HeaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
