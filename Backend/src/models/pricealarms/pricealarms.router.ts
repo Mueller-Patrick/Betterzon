0be394fc1df297ebb809438bfd33ab07358ef9bd
Patrick <50352812+Mueller-Patrick@users.noreply.github.com>
2021-05-13 00:48:56 +0200

    BETTERZON-92: Adding API endpoint to edit (update) price alarms (#44)

--- Backend/src/models/pricealarms/pricealarms.router.ts
+++ Backend/src/models/pricealarms/pricealarms.router.ts
@@ -85,3 +85,45 @@ pricealarmsRouter.post('/create', async (req: Request, res: Response) => {
         res.status(500).send(JSON.stringify({'message': 'Internal Server Error. Try again later.'}));
     }
 });
+
+// PUT pricealarms/update
+pricealarmsRouter.put('/update', async (req: Request, res: Response) => {
+    try {
+        // Authenticate user
+        const session_id = req.body.session_id;
+        const session_key = req.body.session_key;
+        const user_ip = req.connection.remoteAddress ?? '';
+
+        if (!session_id || !session_key) {
+            // Missing
+            res.status(400).send(JSON.stringify({message: 'Missing parameters'}));
+            return;
+        }
+
+        const user = await UserService.checkSession(session_id, session_key, user_ip);
+
+        // Get info for price alarm creation
+        const alarm_id = req.body.alarm_id;
+        const defined_price = req.body.defined_price;
+
+        if (!alarm_id || !defined_price) {
+            // Missing
+            res.status(400).send(JSON.stringify({message: 'Missing parameters'}));
+            return;
+        }
+
+        // Create price alarm
+        const success = await PriceAlarmsService.updatePriceAlarm(alarm_id, user.user_id, defined_price);
+
+        if (success) {
+            res.status(201).send(JSON.stringify({success: true}));
+            return;
+        } else {
+            res.status(500).send(JSON.stringify({success: false}));
+            return;
+        }
+    } catch (e) {
+        console.log('Error handling a request: ' + e.message);
+        res.status(500).send(JSON.stringify({'message': 'Internal Server Error. Try again later.'}));
+    }
+});
