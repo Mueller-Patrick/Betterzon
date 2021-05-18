13615d7142a107f003672f2cc09a532128aeb45f
Patrick Müller <patrick@mueller-patrick.tech>
2021-05-18 21:13:00 +0200

    BETTERZON-95: Adding API endpoint for getting, inserting and updating contact persons

---
+++ Backend/src/models/contact_persons/contact_person.interface.ts
@@ -0,0 +1,9 @@
+export interface Contact_Person {
+    contact_person_id: number;
+    first_name: string;
+    last_name: string;
+    gender: string;
+    email: string;
+    phone: string;
+    vendor_id: number;
+}
