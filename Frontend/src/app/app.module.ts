fc9c7f63cf16631d12c4a77f53d97d7115221d30
Reboooooorn <61185041+Reboooooorn@users.noreply.github.com>
2021-05-20 10:26:10 +0200

    BETTERZON-78 (#39)
    * BETTERZON-31, dependencies.
    
    * BETTERZON-31: Fixing dependencies
    
    * BETTERZON-31,
    BETTERZON-50
    
    info popover and footer had been changed.
    
    * BETTERZON-74
    
    simple top-bar has been created.
    
    * WIP: creating footer using grid.
    
    * BETTERZON-78 adding bottom bar and top bar
    
    * Adding cookieconsent as dependency again since it was removed by a merge
    
    * Adding cookieconsent as dependency again since it was removed by a merge
    
    * Apply suggestions from code review
    
    Switching from single to double quotes
    
    * BETTERZON-78 - grid added, structured as in Adobe XD mockup
    
    Co-authored-by: Patrick Müller <patrick@mueller-patrick.tech>
    Co-authored-by: Patrick <50352812+Mueller-Patrick@users.noreply.github.com>

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -16,10 +16,20 @@ import {NewestPricesListComponent} from './components/newest-prices-list/newest-
 import {FormsModule} from '@angular/forms';
 import {PageNotFoundPageComponent} from './pages/page-not-found-page/page-not-found-page.component';
 import {MatMenuModule} from '@angular/material/menu';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ImprintComponent} from './pages/imprint/imprint.component';
 import {PrivacyComponent} from './pages/privacy/privacy.component';
 import {NgcCookieConsentModule, NgcCookieConsentConfig} from 'ngx-cookieconsent';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {TopBarComponent} from './components/top-bar/top-bar.component';
+import {RouterModule} from '@angular/router';
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from "@angular/material/list";
+import {BottomBarComponent} from './components/bottom-bar/bottom-bar.component';
+
 
 // For cookie popup
 const cookieConfig: NgcCookieConsentConfig = {
@@ -73,7 +83,9 @@ const cookieConfig: NgcCookieConsentConfig = {
         NewestPricesListComponent,
         PageNotFoundPageComponent,
         ImprintComponent,
-        PrivacyComponent
+        PrivacyComponent,
+        TopBarComponent,
+        BottomBarComponent
     ],
     imports: [
         BrowserModule,
@@ -83,7 +95,17 @@ const cookieConfig: NgcCookieConsentConfig = {
         FormsModule,
         MatMenuModule,
         BrowserAnimationsModule,
-        NgcCookieConsentModule.forRoot(cookieConfig)
+        NgcCookieConsentModule.forRoot(cookieConfig),
+        MatSlideToggleModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
+        MatButtonModule,
+        MatIconModule,
+        RouterModule.forRoot([
+            {path: '', component: LandingpageComponent},
+        ]),
     ],
     providers: [],
     bootstrap: [AppComponent]
