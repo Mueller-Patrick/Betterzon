df43febc4553619209e9cf57276a4d3593419e3d
Jegor <georg.reichert.1998@gmail.com>
2021-05-25 08:56:15 +0200

    wip: testing ng-carousel

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -31,7 +31,7 @@ import {MatListModule} from "@angular/material/list";
 import {BottomBarComponent} from './components/bottom-bar/bottom-bar.component';
 import { HotDealsWidgetComponent } from './components/hot-deals-widget/hot-deals-widget.component';
 import { SliderForProductsComponent } from './components/slider-for-products/slider-for-products.component';
-
+import { DragScrollModule } from 'ngx-drag-scroll';
 
 // For cookie popup
 const cookieConfig: NgcCookieConsentConfig = {
@@ -107,6 +107,7 @@ const cookieConfig: NgcCookieConsentConfig = {
         MatListModule,
         MatButtonModule,
         MatIconModule,
+        DragScrollModule,
         RouterModule.forRoot([
             {path: '', component: LandingpageComponent},
         ]),
